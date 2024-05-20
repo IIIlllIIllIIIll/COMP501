249b8a75b6b69b63a3f335611b4193f66785a4aa
Bozhan Liang <lbz@rivensbane.com>
2024-05-20 16:51:53 +1200

    refactor: change js

--- scripts/theme-toggle.js
+++ scripts/theme-toggle.js
@@ -1,45 +1,47 @@
 const storageKey = "theme-preference";
+const light = "light-theme";
+const dark = "dark-theme";
 
 const getColorPreference = () => {
     const preference = localStorage.getItem(storageKey);
-    const query = new URLSearchParams(window.location.search);
-
     if (preference) {
         return preference;
-    } else if (query.has("theme")) {
-        return query.get("theme");
-    } else {
-        return window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark-theme" : "light-theme";
     }
+
+    const query = new URLSearchParams(window.location.search);
+    if (query.has("theme")) {
+        return query.get("theme");
+    }
+
+    return window.matchMedia("(prefers-color-scheme: dark)").matches ? dark : light;
 };
 
 const setPreference = () => {
     const preference = getColorPreference();
     localStorage.setItem(storageKey, preference);
 
-    let element = document.body;
-    element.classList.replace(preference === "light-theme" ? "dark-theme" : "light-theme", preference);
+    const body = document.body;
+    body.classList.replace(preference === light ? dark : light, preference);
 
-    element = document.querySelectorAll(".navbar>li>a");
+    const element = document.querySelectorAll(".navbar>li>a");
     for (let i = 0; i < element.length; i++) {
         const url = new URL(element[i].href);
-        url.searchParams.delete("theme");
-        url.searchParams.append("theme", preference);
+        url.searchParams.set("theme", preference);
         element[i].href = url;
     }
 };
 
 window.onload = () => {
-    document.body.classList.add("light-theme");
+    document.body.classList.add(light);
     localStorage.removeItem(storageKey);
     // set on load so screen readers can get the latest value on the button
     setPreference();
     // now this script can find and listen for clicks on the control
-    document.querySelector(".theme-toggle").addEventListener("click", onClick);
+    document.querySelector(".theme-toggle")?.addEventListener("click", onClick);
 };
 
 const onClick = () => {
     const preference = getColorPreference();
-    localStorage.setItem(storageKey, preference === "light-theme" ? "dark-theme" : "light-theme");
+    localStorage.setItem(storageKey, preference === light ? dark : light);
     setPreference();
 };
