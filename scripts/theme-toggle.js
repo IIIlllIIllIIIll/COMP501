a4492904db2b019986c6ef391ec3cd75b2158820
Bozhan Liang <lbz@rivensbane.com>
2024-05-20 22:20:23 +1200

    fix: timeline theme not change on first load

--- scripts/theme-toggle.js
+++ scripts/theme-toggle.js
@@ -1,13 +1,7 @@
-const storageKey = "theme-preference";
 const light = "light-theme";
 const dark = "dark-theme";
 
 const getColorPreference = () => {
-    const preference = localStorage.getItem(storageKey);
-    if (preference) {
-        return preference;
-    }
-
     const query = new URLSearchParams(window.location.search);
     if (query.has("theme")) {
         return query.get("theme");
@@ -16,32 +10,29 @@ const getColorPreference = () => {
     return window.matchMedia("(prefers-color-scheme: dark)").matches ? dark : light;
 };
 
-const setPreference = () => {
-    const preference = getColorPreference();
-    localStorage.setItem(storageKey, preference);
+const setPreference = (preference) => {
+    document.body.classList.replace(preference === light ? dark : light, preference);
 
-    const body = document.body;
-    body.classList.replace(preference === light ? dark : light, preference);
-
-    const element = document.querySelectorAll(".navbar>li>a");
+    const element = document.querySelectorAll(".local-url");
     for (let i = 0; i < element.length; i++) {
         const url = new URL(element[i].href);
         url.searchParams.set("theme", preference);
         element[i].href = url;
     }
-};
 
-window.onload = () => {
-    document.body.classList.add(light);
-    localStorage.removeItem(storageKey);
-    // set on load so screen readers can get the latest value on the button
-    setPreference();
-    // now this script can find and listen for clicks on the control
-    document.querySelector(".theme-toggle")?.addEventListener("click", onClick);
+    const url = new URL(window.location.href);
+    url.searchParams.set("theme", preference);
+    window.history.replaceState(null, "", url.pathname + url.search + url.hash);
 };
 
 const onClick = () => {
-    const preference = getColorPreference();
-    localStorage.setItem(storageKey, preference === light ? dark : light);
-    setPreference();
+    const preference = getColorPreference() === dark ? light : dark;
+    setPreference(preference);
 };
+
+window.addEventListener("DOMContentLoaded", () => {
+    document.body.classList.add(light);
+    setPreference(getColorPreference());
+    // now this script can find and listen for clicks on the control
+    document.querySelector(".theme-toggle")?.addEventListener("click", onClick);
+});
